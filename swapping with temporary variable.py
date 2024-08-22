f20003cc5b9c4bd695ac85e174fae3c33ede6f9f
Udaykiran96G <118790431+Udaykiran96G@users.noreply.github.com>
2024-08-22 09:23:06 +0530

    Add files via upload

---
+++ swapping with temporary variable.py
@@ -0,0 +1,15 @@
+a = 10
+b = 5
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Using a temporary variable
+temp = a
+a = b
+b = temp
+
+print("After swapping with a temporary variable:")
+print("a =", a)
+print("b =", b)
